e058dec495951d72eec40566cba8fdc3162db9ca
catguo <catguo88@gmail.com>
2018-08-31 09:21:02 -0400

    Basic HTML for Sign Out Button and API Call
    - Sign Out button added to HTML
    - Event Handler for Sign Out Button added
    - DELETE request for User
    - Tested in Dev environment with local API

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -19,7 +19,18 @@ const signIn = function (data) {
   })
 }
 
+const signOut = function () {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
 module.exports = {
   signUp,
-  signIn
+  signIn,
+  signOut
 }
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -31,9 +31,26 @@ const signInFailure = function (error) {
   console.error('signInFailure ran. Error is :', error)
 }
 
+const signOutSuccess = function () {
+  $('#message').text('Signed out successfully')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  console.log('signOutSuccess ran and nothing was returned!')
+  store.user = null
+}
+
+const signOutFailure = function (error) {
+  $('#message').text('Error on sign out')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  console.error('signOutFailure ran. Error is :', error)
+}
+
 module.exports = {
   signUpSuccess,
   signUpFailure,
   signInSuccess,
-  signInFailure
+  signInFailure,
+  signOutSuccess,
+  signOutFailure
 }
