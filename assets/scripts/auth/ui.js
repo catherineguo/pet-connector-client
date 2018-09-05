7db9a015100236ab8e8c6128ef2de68574568966
catguo <catguo88@gmail.com>
2018-09-05 13:38:39 -0400

    Tweaked error alerts

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -4,7 +4,7 @@ const store = require('../store')
 
 const signUpSuccess = function (data) {
   $('#message').html('').removeClass('alert alert-success').removeClass('alert alert-danger')
-  $('#message').html("<p>You've signed up! Now sign in.</p>").addClass('alert alert-success')
+  $('#message').html("<p>You've signed up! Now sign in.</p>").addClass('alert alert-success').show('fast')
   $('#sign-up input').val('')
   $('#sign-in input').val('')
   $('#sign-up-container').hide()
@@ -15,11 +15,11 @@ const signUpSuccess = function (data) {
 const signUpFailure = function () {
   $('#message').html('').removeClass('alert alert-success').removeClass('alert alert-danger')
   if ($('#password').val() !== $('#confirm-password').val()) {
-    $('#message').html("<p>Sorry, your passwords don't match.</p>").addClass('alert alert-danger')
+    $('#message').html("<p>Sorry, your passwords don't match.</p>").addClass('alert alert-danger').show('fast')
     $('#password').val('')
     $('#confirm-password').val('')
   } else {
-    $('#message').html('<p>Sorry, something went wrong. Please try again.</p>').addClass('alert alert-danger')
+    $('#message').html('<p>Sorry, something went wrong. Please try again.</p>').addClass('alert alert-danger').show('fast')
     $('#sign-up input').val('')
   }
 }
@@ -35,13 +35,13 @@ const signInSuccess = function (data) {
 
 const signInFailure = function () {
   $('#message').html('').removeClass('alert alert-success').removeClass('alert alert-danger')
-  $('#message').html('<p>Oops, try again.</p>').addClass('alert alert-danger')
+  $('#message').html('<p>Oops, try again.</p>').addClass('alert alert-danger').show('fast')
   $('#sign-in input').val('')
 }
 
 const signOutSuccess = function () {
   $('#message').html('').removeClass('alert alert-success').removeClass('alert alert-danger')
-  $('#message').html('<p>Successfully signed out.</p>').addClass('alert alert-success')
+  $('#message').html('<p>Successfully signed out.</p>').addClass('alert alert-success').show('fast').delay(1000).hide('slow')
   $('.landing-page').show()
   $('#navbar').hide()
   $('.dashboard-layout').hide()
@@ -51,7 +51,7 @@ const signOutSuccess = function () {
 
 const signOutFailure = function () {
   $('#message').html('').removeClass('alert alert-success').removeClass('alert alert-danger')
-  $('#message').html('<p>Sign out failed.</p>').addClass('alert alert-danger')
+  $('#message').html('<p>Sign out failed.</p>').addClass('alert alert-danger').show('fast')
 }
 
 const changePasswordSuccess = function () {
